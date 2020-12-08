4529e81da8cd330e44aeaa5eb7f6010712e8ba9b
melike-eryilmaz <41503266+melike-eryilmaz@users.noreply.github.com>
2020-12-08 14:34:57 +0300

    if şart bloğu örnekleri eklenmiştir.

---
+++ conditionalBlocks.py
@@ -0,0 +1,11 @@
+dolarDun=7.65
+dolarBugun=7.75
+
+if dolarDun<dolarBugun:
+  print("Dolar artıyor..")
+
+if dolarDun > dolarBugun:
+  print("Dolar düşüyor..")
+
+if dolarDun == dolarBugun:
+  print("Dolar sabit kalıyor..")
