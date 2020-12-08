ba542d6aa28531e56bb72b440358adb2318847e2
melike-eryilmaz <41503266+melike-eryilmaz@users.noreply.github.com>
2020-12-08 14:36:51 +0300

    if,elif,else şart blokları örneği eklenmiştir.

---
+++ conditionalBlocks3.py
@@ -0,0 +1,11 @@
+dolarDun=7.65
+dolarBugun=7.85
+
+if   dolarDun < dolarBugun :
+  print("Dolar artıyor..")
+elif dolarDun > dolarBugun :
+  print("Dolar azalıyor..")
+else:
+  print("Dolar yerinde sayıyor..")
+
+print("İşlemler sonlanıyor..")
