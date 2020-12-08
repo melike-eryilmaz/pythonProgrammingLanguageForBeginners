943ee64a13df5ea66a75f5a86bd088806f4264e5
melike-eryilmaz <41503266+melike-eryilmaz@users.noreply.github.com>
2020-12-08 14:44:24 +0300

    Python dizi yapısı örneği eklenmiştir.

---
+++ arrays.py
@@ -0,0 +1,16 @@
+krediler=["İhtiyaç Kredisi","Maaşını Halkbank İle Alanlara Özel","Emeklilik Kredisi","Araba Kredisi"]
+
+
+print(krediler)
+print(krediler[0])
+print(krediler[1])
+print(krediler[2])
+print(krediler[3])
+
+#Dizinin uzunluğunu verir.
+print(len(krediler))
+
+krediler[0]="Çabuk Kredi"
+
+print(krediler)
+
