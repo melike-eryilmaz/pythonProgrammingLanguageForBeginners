3c174d7954d2675d97390867657b93a54ceb8b92
melike-eryilmaz <41503266+melike-eryilmaz@users.noreply.github.com>
2020-12-08 14:38:05 +0300

    if,elif,else şart blokları örneği eklenmiştir.

---
+++ conditionalBlocks4.py
@@ -0,0 +1,9 @@
+birinciSayi = 9
+ikinciSayi  = 6
+
+if birinciSayi > ikinciSayi:
+  print( "BirinciSayi büyüktür.")
+elif ikinciSayi > birinciSayi :
+  print("İkinciSayi büyüktür.")
+else:
+  print("Sayılar eşittir.")
