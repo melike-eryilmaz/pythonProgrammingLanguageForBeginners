c8a5184e72f1d59bc3cb8da061d75da88f119776
melike-eryilmaz <41503266+melike-eryilmaz@users.noreply.github.com>
2020-12-08 14:45:11 +0300

    Array yapısı örneği eklenmiştir.

---
+++ arrays2.py
@@ -0,0 +1,11 @@
+ogrenciler=["Melike","Duygu","Elif","Zafer"]
+
+print(ogrenciler[0])
+print(ogrenciler[1])
+print(ogrenciler[2])
+print(ogrenciler[3])
+
+print(len(ogrenciler))
+ogrenciler[1]="Elvin"
+
+print(ogrenciler)
