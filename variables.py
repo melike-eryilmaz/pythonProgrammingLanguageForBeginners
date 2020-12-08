2e6d3f378c13bd5ac5dd87484454f06b3f3f6ad5
melike-eryilmaz <41503266+melike-eryilmaz@users.noreply.github.com>
2020-12-08 14:33:30 +0300

    Python veri tipleri örnekleri verilmiştir.

---
+++ variables.py
@@ -0,0 +1,22 @@
+# Verileri değişkenlerde tutarız.
+haber="Saaat 9 s kadar sokağa çıkma yasaklandı."
+vade = 12
+faizOranı=1.58
+print(haber)
+print(type(faizOranı))
+# type fonksiyonu ile verilerin hangi türden olduğunu öğrenebiliriz.
+
+mesaj="Hoşgeldin"
+musteriAdı="Melike"
+musteriSoyadı="Eryılmaz"
+# Program içerinde bir ifadeyi birden çok yerde kullanacaksak bunu bir değişkene atarız.
+# Böylece değişkenin değerini değiştirmek istersek sadece tanımlandığı yerde değiştirmemiz yeterli olur.
+print("Ad Soyad:",musteriAdı+" "+musteriSoyadı)
+sonucMesaji=mesaj+" "+musteriAdı+" "+musteriSoyadı
+print("Mesaj:",sonucMesaji)
+
+birinciSayı=12
+ikinciSayi=14
+print("Toplam:",birinciSayı+ikinciSayi)
+
+print("Mesaj",sonucMesaji)
