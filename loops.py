9065ebfa96978757175d4a725c0735f50eb2b995
melike-eryilmaz <41503266+melike-eryilmaz@users.noreply.github.com>
2020-12-08 14:47:35 +0300

    Python for döngüsü temel yapısını gösteren örnek eklenmiştir.

---
+++ loops.py
@@ -0,0 +1,16 @@
+krediler=["İhtiyaç Kredisi","Maaşını Halkbank İle Alanlara Özel","Emeklilik Kredisi","Araba Kredisi"]
+
+for i in krediler:
+   print(i)
+
+for i in range(10):
+  print(i)
+
+for i in range(len(krediler)):
+  print(krediler[i])
+
+for i in range(3,10):
+  print(i)
+
+for i in range(0,10,2):
+  print(i)
