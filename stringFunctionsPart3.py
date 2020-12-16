eb783810500fed2847dec7f57b728682d8479e90
melike-eryilmaz <41503266+melike-eryilmaz@users.noreply.github.com>
2020-12-16 15:43:49 +0300

    Create stringFunctionsPart3.py
    len() fonksiyonu örnek kodları paylaşılmıştır.

---
+++ stringFunctionsPart3.py
@@ -0,0 +1,8 @@
+#12/12/2020 len() fonksiyonu
+#len() fonksiyonu geriye dizinin uzunluğunu döndürür.
+message = 'Hello World'
+
+print(len(message))
+
+#Dizinin son karakterini bulmak için substing ve len methodlarını kullanabiliriz.
+print(message[len(message)-1:len(message)])
