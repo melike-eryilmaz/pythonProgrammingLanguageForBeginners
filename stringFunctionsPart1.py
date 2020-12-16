cc8194a432188ea6db733a5c223440b452de5559
melike-eryilmaz <41503266+melike-eryilmaz@users.noreply.github.com>
2020-12-16 14:56:51 +0300

    Create stringFunctionsPart1.py

---
+++ stringFunctionsPart1.py
@@ -0,0 +1,12 @@
+#Python da stringler ile işlemler yapmak için özel string fonksiyonları vardır.
+#Bunlardan bazıları upper() (string ifadeyi büyük harfe çevirir) ve lower() (string ifadeyi küçük harfe çevirir.
+
+firstString = 'Python is awesome!'
+secondStrşng='PYTHON is Awesome!'
+
+
+if(firstString.lower() == secondStrşng.lower()):
+    print('Two string are same')
+else:
+    print('Two string are not same')
+    
