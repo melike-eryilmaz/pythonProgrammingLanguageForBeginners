f52a959317fa9c0a35b6c80d0fecfcf42f1783f5
melike-eryilmaz <41503266+melike-eryilmaz@users.noreply.github.com>
2020-12-16 15:56:17 +0300

    Create stringFunctionsPart4.py
    replace() fonksiyonu örnek kodları eklenmiştir.

---
+++ stringFunctionsPart4.py
@@ -0,0 +1,11 @@
+#12/12/2020 replace() fonksiyonu
+
+#replace() fonksiyonu string içerisindeki karakterleri veya karakter dizilerini yeniden adlandırmamızı sağlar.
+
+message = 'Hello World!'
+
+print(message.replace('o','ö'))
+
+message = message.replace('e','t') #message değişkeninin değerinin değişmesi için bu şekilde atama yapmalıyız.
+
+print(message)
