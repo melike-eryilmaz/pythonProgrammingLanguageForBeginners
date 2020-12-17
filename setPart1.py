351085c0faf578154b305bb622ac7aa449906944
melike-eryilmaz <41503266+melike-eryilmaz@users.noreply.github.com>
2020-12-17 15:57:23 +0300

    Create setPart1.py
    Python setler temel yapı ile alakalı örnek kodlar paylaşılmıştır.

---
+++ setPart1.py
@@ -0,0 +1,13 @@
+#17/12/2020 setler
+
+#Set ler listelere benzerdir.
+#En önemli özelliği indexsiz ve sırasız elemanlardan oluşmasıdır.
+#Veri tekrarı olamaz .Tüm elemanlar eşsizdir.(unique)
+#Set veri yapısı çok performanslı bir data sağlar.(çok hızlı)
+
+#set of integers
+my_set = {1,2,3}
+print(my_set)
+
+#set of mixed data types
+my_set = {1.0,'Hello',(1,2,3)}
