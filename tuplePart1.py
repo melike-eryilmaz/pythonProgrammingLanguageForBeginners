a91b77dc37b8ad960379d6eacf6bc99f549bb9f5
melike-eryilmaz <41503266+melike-eryilmaz@users.noreply.github.com>
2020-12-17 13:22:01 +0300

    Create tuplePart1.py
    Tuple veri yapısı temeli örnekleri eklenmiştir.

---
+++ tuplePart1.py
@@ -0,0 +1,19 @@
+#17/12/2020 Tuple veri yapısı
+#Tuple lar listelerle benzer yapıdadır .
+#Listelerden farkı listelerde elemanları değiştirebiliyorken tuple da değiştirmek söz konusu değildir.
+#Tuple veri yapısı performanslı data sağlar.
+
+#empty tuple 
+my_tuple = ()
+print(my_tuple)
+
+#tuple having integers 
+my_tuple = (1,2,3,4)
+print(my_tuple)
+
+#tuple with mixed datatypes
+my_tuple = (1,'Hello',1.4)
+print(my_tuple)
+
+#nested tuple 
+my_tuple = ('mouse',[8,4,6],[1,2,3])
