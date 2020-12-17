30f7bd7c38c558e183e98864a0abac0e06236075
melike-eryilmaz <41503266+melike-eryilmaz@users.noreply.github.com>
2020-12-17 10:57:05 +0300

    Create arraysPart1.py
    Temel dizi yapısı örnekleri paylaşılmıştır.

---
+++ arraysPart1.py
@@ -0,0 +1,21 @@
+
+"""
+Python Veri Yapıları
+
+Listeler :
+Python da en çok kullanılan veritiplerinden biri listelerdir.
+Sıralı ve indexli bir yapıya sahiptir.
+Listeler köşeli parantez ile belirtilir ve birden fazla veri tutabilirler.
+liste[0] şeklinde listelerin elemanlarına erişebiliriz.
+Liste içerisinde başka bir liste eleman olarak yer alabilir.
+
+"""
+
+#empty array
+my_list = []
+
+#list of integers 
+my_list = [1,2,3,4]
+
+#list with mixed data types
+my_list = [1,'Hello',3.4]
