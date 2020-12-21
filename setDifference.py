61524a1ccb2c05018a09e1cbd9f2048f5e7c35fb
melike-eryilmaz <41503266+melike-eryilmaz@users.noreply.github.com>
2020-12-21 11:26:39 +0300

    Create setDifference.py

---
+++ setDifference.py
@@ -0,0 +1,8 @@
+# 21/12/2020 setDifference
+# setDifference iki ayrı set de birinde olup diğerinde olmayan elemanları almamızı sağlar.
+setA = {1,2,3,4,5}
+setB = {3,4,5}
+
+print(setA - setB)
+print(setA.difference(setB))
+print(setB.difference(setA))
