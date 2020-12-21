cfc7df8f75722e0214b52255b0cebba544f58e7a
melike-eryilmaz <41503266+melike-eryilmaz@users.noreply.github.com>
2020-12-21 11:18:42 +0300

    Create setIntersection.py

---
+++ setIntersection.py
@@ -0,0 +1,8 @@
+# 21/12/2020 setIntersection 
+# setIntersection iki ayrı set de ortak olan elemanları almamızı sağlar.
+setA = {1,2,3,4,5}
+setB = {3,4,5}
+
+print(setA & setB)
+print(setA.intersection(setB))
+print(setB.intersection(setA))
