3b795c2a24642f3c800d1f95c777051e6167a5d0
melike-eryilmaz <41503266+melike-eryilmaz@users.noreply.github.com>
2020-12-21 11:10:08 +0300

    Create setUnion.py
    setUnion örnekleri eklendi.

---
+++ setUnion.py
@@ -0,0 +1,9 @@
+# 21/12/2020 setunion 
+# setUnion iki ayrı seti ortak elemanları bir kez alarak birleştirmemizi sağlar.
+#Birleştirdikten sonra kendi algoritmasına göre sıralar.
+
+numberSet = {1,2,3}
+numberSet1 = {2,3,5}
+
+print(numberSet | numberSet1)
+print:(numberSet.union(numberSet1))
