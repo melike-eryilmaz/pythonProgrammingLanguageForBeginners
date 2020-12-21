e6d07ca90c3c838dd4d3bdf6af22cb848160219f
melike-eryilmaz <41503266+melike-eryilmaz@users.noreply.github.com>
2020-12-21 14:23:15 +0300

    Create conditionalBlocks7.py

---
+++ # 21/12/conditionalBlocks7.py
@@ -0,0 +1,17 @@
+# 21/12/2020 Conditional Blocks 
+
+sayi1 = 20
+sayi2 = 30
+
+if sayi1 < sayi2:
+    print("bir şey")
+    print("ikinci bir şey")
+print("şart bloğu içinde değil")
+
+
+
+
+if sayi1 >sayi2:
+    print("bir şey")
+    print("ikinci bir şey")
+print("şart bloğu içinde değil")
