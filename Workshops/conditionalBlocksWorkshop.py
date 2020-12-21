239e9a0a1d65b9e401c930527d60655202520792
melike-eryilmaz <41503266+melike-eryilmaz@users.noreply.github.com>
2020-12-21 14:59:22 +0300

    Create conditionalBlocksWorkshop.py
    Girilen sayının sıfırdan büyük,küçük,eşit koşullarını kontrol eden örnek program kodu eklenmiştir.

---
+++ Workshops/conditionalBlocksWorkshop.py
@@ -0,0 +1,11 @@
+# Conditional blocks workshop 21/12/2020
+
+sayi = int(input('Sayi giriniz:'))
+
+if sayi == 0:
+    print('Sayı sıfırdır.')
+elif sayi > 0:
+    print('Sayı sıfırdan büyüktür.')
+else:
+    print('Sayı sıfırdan küçüktür.')
+
