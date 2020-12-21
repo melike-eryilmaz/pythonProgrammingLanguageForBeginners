17601755c431b87b069ddfe0e78850d46bea5802
melike-eryilmaz <41503266+melike-eryilmaz@users.noreply.github.com>
2020-12-21 14:35:27 +0300

    Create conditionalBlocks8.py

---
+++ conditionalBlocks8.py
@@ -0,0 +1,17 @@
+# 21/12/2020 Conditional Blocks
+
+lights = ['red','yellow','green']
+
+# currentLight = lights[0]
+# currentLight = lights[1]
+currentLight = lights[2]
+print(currentLight)
+
+if currentLight == 'red':
+    print('STOP!')
+
+if currentLight == 'yellow':
+    print('READY!')
+    
+if currentLight == 'green':
+    print('GO!')
