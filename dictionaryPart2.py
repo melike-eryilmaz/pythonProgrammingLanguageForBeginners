0dc15c866e8f90498ea01a74cec21728c4e59011
melike-eryilmaz <41503266+melike-eryilmaz@users.noreply.github.com>
2020-12-21 12:05:31 +0300

    Create dictionaryPart2.py

---
+++ dictionaryPart2.py
@@ -0,0 +1,18 @@
+# 21/12/2020 dictionary
+# 
+
+sozluk = {
+    'book' :'kitap',
+    'table':'masa'
+    
+    }
+
+sozluk['book'] = 'kitap 1'
+sozluk['fruit'] = 'meyve'
+
+print(sozluk)
+
+del(sozluk['book'])
+
+print(len(sozluk))
+print(sozluk)
