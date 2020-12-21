e41c9c4d91f348dae7726c267cad7673fa713446
melike-eryilmaz <41503266+melike-eryilmaz@users.noreply.github.com>
2020-12-21 15:48:50 +0300

    Create forIteration.py

---
+++ forIteration.py
@@ -0,0 +1,13 @@
+# 21/12/2020 for iteration 
+
+#list of numbers
+numbers = [1,2,3,4,5]
+
+# variable to store sum
+sum = 0
+
+for value in numbers:
+    sum = sum + value
+    
+
+print('The sum is ',sum)
