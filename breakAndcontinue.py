3034b2999b402dcea3e409f24d22299cec83bf30
melike-eryilmaz <41503266+melike-eryilmaz@users.noreply.github.com>
2020-12-22 10:52:59 +0300

    Create breakAndcontinue.py

---
+++ breakAndcontinue.py
@@ -0,0 +1,27 @@
+# 22/12/2020 break continue
+# break bağlı bulunulan döngü için döngüyü bitirmek demektir.
+# Örnek kullanım olarak :Uygulamanıza bir veri geldi ,eğer o veri gelirse bir sonraki adımları
+# çalıştırmak istemiyorsanız ,uygulamayı orda kırmak için break kullanırız.
+
+sehirler = ['Ankara','İstanbul','İzmir']
+
+
+# Aşağıdaki gibi gelen veri İstanbul olunca işlemlere devam etmek istemiyorsak kullanırız.
+# Dikkat for iteration sonlandı diğer sehir için dönmedi.
+for sehir in sehirler :
+    if sehir == 'İstanbul':
+        break
+     
+    print('Şehir :',sehir)
+    print('*************')
+
+
+# continue ise döngüde sadece o anki değerde işlemleri geçip diğerlerinde devam eder.
+# break te karşılaşılan değerden sonra diğer değerlere bakılmamıştı ,continue da değerler tek tek değerlendirilir.
+
+for sehir in sehirler :
+    if sehir == 'İstanbul':
+        continue
+     
+    print('Şehir :',sehir)
+    print('*************')
