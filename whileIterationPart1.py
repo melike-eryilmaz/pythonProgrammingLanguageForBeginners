3825fdecb6971b1c4f298cf283a0791e95dbb162
melike-eryilmaz <41503266+melike-eryilmaz@users.noreply.github.com>
2020-12-22 10:27:13 +0300

    Create whileIterationPart1.py

---
+++ whileIterationPart1.py
@@ -0,0 +1,15 @@
+# 22/12/2020 While Iteration
+
+# for dongüsü gibi belirli bir şartı sağlayana kadar döngü kurmak için kullanılır.
+# Genellikle for döngüsü tercih edilir.
+
+
+sum = 0
+i = 1
+
+while i <= 10:
+    sum = sum +i
+    i = i+1
+    
+
+print('The sum is ',sum)
