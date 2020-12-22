d2e2c9007b60ba87faca890f97befc4dd96ddb81
melike-eryilmaz <41503266+melike-eryilmaz@users.noreply.github.com>
2020-12-22 11:04:44 +0300

    Create forIterationPart3Range.py

---
+++ forIterationPart3Range.py
@@ -0,0 +1,17 @@
+# 22/12/2020 for iteration range
+
+# 0 dan başlayıp 100 kez döner 99 da biter.
+for x in range(100):
+    print(x)
+    
+#Başlangıç ve bitiş değeri verilebilir. 
+for x in range(1,100):
+    print(x)
+    
+# Başlangıç bitiş ve artış değeri verilebilir.
+for x in range(1,100,2):
+    print(x)
+    
+    
+for x in range(2,100,2):
+    print(x)
