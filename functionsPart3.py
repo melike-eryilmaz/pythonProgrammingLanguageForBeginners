5104f452ff4518eeef2ba77017a58e3f7300af6c
melike-eryilmaz <41503266+melike-eryilmaz@users.noreply.github.com>
2020-12-22 16:55:00 +0300

    Create functionsPart3.py

---
+++ functionsPart3.py
@@ -0,0 +1,10 @@
+# Fonksiyonlar geriye değer döndüren ve geriye değer döndürmeyen şeklinde 2 ye ayrılır.
+
+#Geriye değer dönen fonksiyon örneği aşağıdadır.
+def dikucgenalanihesapla(a,b):
+    return a*b/2;
+
+
+alan=dikucgenalanihesapla(3,4)
+print(alan)
+
