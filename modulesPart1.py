5d43d704f5ec8f721d1889fa1572f45321e235d0
melike-eryilmaz <41503266+melike-eryilmaz@users.noreply.github.com>
2020-12-25 22:52:42 +0300

    Create modulesPart1.py

---
+++ modulesPart1.py
@@ -0,0 +1,20 @@
+""" 25/12/2020 Module Yapısı 
+    Module yapısı bir dosyadan diğerine erişemek gibi diğer modülleri kullanabilmezi sağlar.
+
+
+"""
+
+def sum(number1,number2):
+    print('Sum is :',number1+number2)
+
+
+def diff(number1,number2):
+    print('Diff is :',number1-number2)
+
+
+customer = {
+            'firstName':'Melike',
+            'lastName': 'Eryılmaz'
+    
+    
+            }
