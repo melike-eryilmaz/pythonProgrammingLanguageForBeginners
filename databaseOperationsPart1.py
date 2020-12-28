8004facb641888f1207c82326c58b8fa9d3d3527
melike-eryilmaz <41503266+melike-eryilmaz@users.noreply.github.com>
2020-12-28 11:08:31 +0300

    Create databaseOperationsPart1.py

---
+++ databaseOperationsPart1.py
@@ -0,0 +1,13 @@
+""" 28/12/2020 Database Operations
+"""
+
+"""
+Veritabanı nedir ?
+Bir sosyal medyaya yada uygulamaya üye oluyorsunuz diyelim.
+Girişte sizden alınan ad,soyad,telefon gibi bilgilerin tutulduğu sisteme vertabanı sistemleri denir.
+Örneğin kullanıcı bilgileri Users tablosunda kullanıcıya ait fotoğraflar Photos tablosunda tutulsun.
+Her kullanıcının bir fotoğrafı olabileceği için Users ve Photos tabloları birbiriyle ilişkilidir.
+Bu yapıdaki veritabanı sistemlerine ilişkisel veritabanı sistemleri denir.
+İlişkisel veri tabanı sistemleri yaygın olarak kulllanılır.
+Özetle veritabanı verilerin saklandığı yapılardır,ilişkisel veritabanı sistemleri de mevcuttur.
+"""
